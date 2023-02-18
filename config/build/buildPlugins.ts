cc2101b380a60ead1ed29206c1134678c99e062a
Maksim Kaporikov <makapori@gmail.com>
2023-02-18 18:44:45 +0700

    fix: dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,15 +6,23 @@ import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 export default function ({ paths, isDev }: BuildOptions): WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({ template: paths.html }),
     new MiniCssExtractPlugin({ filename: 'css/[name].[fullhash].css', chunkFilename: 'css/[name].[fullhash].css' }),
     new ProgressPlugin(),
     new DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
-    }),
-    new ReactRefreshPlugin({ overlay: false }),
-    new HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({ openAnalyzer: false })
+    })
+
   ]
+
+  if (isDev) {
+    plugins.push(
+      new BundleAnalyzerPlugin({ openAnalyzer: false }),
+      new ReactRefreshPlugin({ overlay: false }),
+      new HotModuleReplacementPlugin()
+    )
+  }
+
+  return plugins
 }
